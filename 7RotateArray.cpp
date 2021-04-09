29767b09bdd0532dc08827ac9d40488e0e5d8053
Saksham Agarwal <skshamagarwal@gmail.com>
2021-04-09 22:31:47 +0530

    day1

---
+++ 7RotateArray.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void rotate(vector<ll> &ar, ll l){
+    ll temp = ar[ar.size()-1];
+    ar.pop_back();
+    ar.insert(ar.begin(), 1, temp);
+}
+
+int main(){
+    ll l, t;
+    cout<<"Enter Length of array:: ";
+    cin>>l;
+    vector<ll> ar;
+    cout<<"Enter array elements space seperated:: ";
+    for (ll i=0; i<l; i++){
+        ll ip;
+        cin>>ip;
+        ar.push_back(ip); 
+    }
+    cout<<"How many Times you want to rotate the vector:: ";
+    cin>>t;
+    for (ll i=0; i<t; i++){
+        rotate(ar, l);
+    }
+
+    cout<<"Array after "<<t<<" rotations :: ";
+    for (int i=0; i<l; i++){
+        cout<<ar[i]<<" ";
+    }
+}
